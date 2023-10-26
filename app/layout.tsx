edc45f003600f89664819cf7445ee97dc7eefdd3
kaoru181206 <karats24.kaoru@gmail.com>
2023-10-26 19:16:48 +0900

    サインインモーダルの作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 
 export const metadata: Metadata = {
@@ -21,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />  
+        <ClientOnly>
+          <Modal isOpen  />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
